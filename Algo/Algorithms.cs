b4c824cc8f55d44253fb183a796dbcedb910c57e
Raza Rajwani <r.rajwani@UBT.EDU.SA>
2021-09-13 16:05:53 +0300

    CountTallestCandlesBlownOut

---
+++ Algo/Algorithms.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo
+{
+    public static class Algorithms
+    {
+        public static void DistributeEqual(int[] arr)
+        {
+            int[] possiblities = new int[5];
+            int minimum = arr.Min();
+
+            for (int i = 0; i < possiblities.Length; i++)
+            {
+                foreach (var k in arr)
+                {
+                    int diff = k - minimum;
+                    int stepsRequired = diff / 5 + (diff % 5) / 2 + ((diff % 5) % 2) / 1;
+                    possiblities[i] += stepsRequired;
+                }
+
+                minimum--;
+
+            }
+            Console.WriteLine($"{Environment.NewLine}Algorithm = DistributeEqual{Environment.NewLine}Possiblities: {possiblities.Min()} ");
+        }
+
+
+        public static void findDuplicatesConstantSpace(int[] nums)
+        {
+            List<int> resultSet = new List<int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                // Get the index, the element corresponds to
+                int index = Math.Abs(nums[i]) - 1;
+                if (index < nums.Length)
+                {
+                    int val = nums[index];
+
+                    // If the number is already negative, it means we are encountering it twice
+                    if (val < 0)
+                    {
+                        resultSet.Add(index + 1);
+                    }
+                    // Flip the number at the index to negative
+                    nums[index] *= -1;
+                }
+
+            }
+
+            Console.WriteLine($"{Environment.NewLine}Algorithm = FindDuplicatesConstantSpace {Environment.NewLine}Duplicates: {string.Join(", ", resultSet.ToArray())}");
+
+        }
+
+
+        public static void CountTallestCandlesBlownOut(int[] candles)
+        {
+            // Get the height of the tallest candle
+            int tallestCandle = candles.Max();
+            foreach (int candle in candles)
+            {
+                if (candle>= tallestCandle)
+                {
+                    tallestCandle = candle;
+                }
+            }
+
+
+            // Count how many tallest candles are present
+            int candleCount = 0;
+            foreach (int candle in candles)
+            {
+                if (candle == tallestCandle)
+                {
+                    candleCount++;
+                }
+            }
+
+            Console.WriteLine($"{Environment.NewLine}" +
+                $"Algorithm = CountTallestCandlesBlownOut {Environment.NewLine}" +
+                $"Total {candleCount} candle(s) count of candle {tallestCandle}");
+
+        }
+    }
+}
--- Algo/Program.cs
+++ Algo/Program.cs
@@ -6,7 +6,12 @@ namespace Algo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+
+            Algorithms.findDuplicatesConstantSpace(new int[] { 13, 15, 3, 2, 7, 8, 2, 3, 1, 20, 2 });
+            Algorithms.DistributeEqual(new int[] { 2, 6, 6, 6 });
+            Algorithms.CountTallestCandlesBlownOut(new int[] { 4, 1, 1, 4, 6, 3, 4, 6, 6 });
+
             Console.ReadLine();
         }
     }
